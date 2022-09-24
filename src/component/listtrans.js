fe18dbc02084527a5759fb213a993519fb2ab5cc
penerus programmer <engkoskostaman759@gmail.com>
2022-09-25 03:25:17 +0700

    depoloy

--- src/component/listtrans.js
+++ src/component/listtrans.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Dropdown from 'react-bootstrap/Dropdown'
 import Navbar from 'react-bootstrap/Navbar';
-import Link from 'react-bootstrap';
 import image1 from '../image/dumbflix.png';
 import image2 from '../image/Ellipse 1.png';
 import image3 from '../image/Vector.png';
